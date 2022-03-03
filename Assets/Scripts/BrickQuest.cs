495ee5dbc8793a5d043315b0e05d23d5008d4260
Wridzer Kamphuis <wridzer12@gmail.com>
2022-03-03 15:10:58 +0100

    latest update

--- Assets/Scripts/BrickQuest.cs
+++ Assets/Scripts/BrickQuest.cs
@@ -7,18 +7,21 @@ public class BrickQuest : MonoBehaviour
     private int brickCount = 0;
     [SerializeField] private GameObject finalBrickHolder;
     [SerializeField] private QuestNPC npc;
+    [SerializeField] private GameObject oldHouse, newHouse;
 
     // Update is called once per frame
     void Update()
     {
-        if(brickCount == 2)
+        /*if(brickCount == 2)
         {
             finalBrickHolder.SetActive(true);
-        }
+        }*/
 
-        if (brickCount == 3)
+        if (brickCount == 12)
         {
             npc.GetComponent<QuestNPC>().QuestComplete();
+            oldHouse.SetActive(false);
+            newHouse.SetActive(true);
         }
     }
 
